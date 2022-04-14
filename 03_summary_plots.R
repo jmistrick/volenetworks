694453e402ea80e3faf837506c9d34b34b927bde
Janine Mistrick <jmistrick@gmail.com>
2022-04-14 15:44:21 -0500

    ANOVAs comparing residents and nonresidents

--- 03_summary_plots.R
+++ 03_summary_plots.R
@@ -347,3 +347,112 @@ single_vs_recap %>% ggplot(aes(x=capture.type, y=count, fill=helm_trt)) +
 
 
 
+
+####################### residents (used for MCP analysis) ####################################
+
+#number of residents per grid (over 2021 season)
+resbygrid <- fulltrap %>% 
+  group_by(site) %>%
+  count(tag) %>%
+  filter(n>4) %>%
+  count(site)
+
+resbygrid <- resbygrid %>% left_join(grid_trts)
+
+#identities of the residents
+IDresbygrid <- fulltrap %>% 
+  group_by(site) %>%
+  count(tag) %>%
+  filter(n>4)
+
+#number single caps per grid (over 2021 season)
+singlecapbygrid <- fulltrap %>% 
+  group_by(site) %>%
+  count(tag) %>%
+  filter(n==1) %>%
+  count(site)
+
+singlecapbygrid <- singlecapbygrid %>% left_join(grid_trts)
+
+#identities of the single caps
+IDsinglecapbygrid <- fulltrap %>% 
+  group_by(site) %>%
+  count(tag) %>%
+  filter(n==1)
+
+
+####################### some anovas for giggles ######################
+
+resbygrid$trt <- as.factor(resbygrid$trt)
+singlecapbygrid$trt <- as.factor(singlecapbygrid$trt)
+
+# data = resbygrid
+data = singlecapbygrid
+
+#plot
+data %>% ggplot(aes(x=trt, y=n, fill=trt)) + 
+  geom_boxplot() +
+  labs(title = "THIS IS THE TITLE")
+
+### number of resident voles (captured 5 or more times) is not different by treatment type
+### number of singlcaptures is not different by treatment type (though boxplots look more so)
+    #unless you run the Kruskal-Wallis, then control_deworm and supp_x's are different
+
+
+mod <- aov(n ~ trt, data=data)
+mod
+
+##oof, let's check them assumptions
+#data are normally distributed and the variance across groups are homogeneous
+
+#plot residuals vs. fitted - for homogeneity of variance
+#looking for outliers (several)
+#but the red line fits the 0 pretty well
+#however, there is some fanning
+plot(mod, 1)
+
+#or use the Levene's test in car package - less sensitive to departures from normal dist
+library(car)
+leveneTest(n ~ trt, data=data)
+#check the output to see if p-value is less than the significance level of 0.05. 
+#IF pvalue is NOT significant: This means that there is no evidence to suggest that the 
+#variance across groups is statistically significantly different. 
+#We can assume the homogeneity of variances in the different treatment groups.
+## IF pvalue is significant, this suggests variances are not the same across groups 
+
+
+#classical ANOVA has a strict assumption of equal variance for all groups
+# Welch one-way test DOES NOT require equal variance
+# Welch test is for normally distributed data that violates assumption of homogeneity of variance
+
+#ANOVA with no assumption of equal variances
+oneway.test(n ~ trt, data=data)
+
+#pairwise t-tests with no assumption of equal variances
+pairwise.t.test(data$n, data$trt,
+                p.adjust.method = "BH", pool.sd = FALSE)
+########### NOT SURE WHAT THE p.adjust.method means CHECK THIS >> ?p.adjust
+
+#check normality
+plot(mod, 2) #if the points fall mostly along the line (ignoring outliers) - you're good
+#the Skapiro-Wilk test can also test normality from the residuals of the ANOVA
+residuals <- residuals(mod)
+shapiro.test(x=residuals) #as long as the pvalue is large, you're good
+
+
+###### Kruskal-Wallis rank-sum test is a non-parametric alternative to ANOVA
+# non-parametric meaning that it doesn't assume your data come from a specific distribution
+#(ie they don't have to be normally distributed)
+#commonly used if you have 2 or more levels - for only 2 levels, use Mann-Whitney
+# ? also does not assume equal variance // but another source said it was unstable when variance was not equal
+
+#Kruskal-Wallis rank-sum test
+kruskal.test(n ~ trt, data=data)
+
+#if result is significant, a common post-hoc test is the Dunn test - this again allows for pvalue adjustments to
+# "control the familywise error rate or the false discovery rate"
+# Zar 2010 states that Dunn test is appropriate for groups with unequal numbers of observations
+#Dunn test is a post-hoc non-parametric test but can be VERY conservative
+
+library(FSA) #package for dunnTest() function
+dunnTest(n ~ trt, data=data, method="bh")
